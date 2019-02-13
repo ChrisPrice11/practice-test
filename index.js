62caff826dd7fe6731c718387145e7887707c457
Christopher.Price <christopher.price@target.com>
2019-02-12 20:38:52 -0600

    first question

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.sendFile(__dirname + "/" + "index.htm")
+//  response.sendFile(__dirname + "/" + "index.html")
+response.sendFile("index.html")
 })
 
 app.listen(app.get('port'), function() {
